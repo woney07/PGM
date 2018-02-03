7cd258abda54ec310d7358f6247958a43af8cfe6
JONG WON LEE <woney07@nate.com>
2018-02-03 09:22:48 +0900

    Xamarin_Forms First Commit

---
+++ Xamarin_Forms/Xamarin_to_WinPhone/Xamarin_to_WinPhone/Xamarin_to_WinPhone/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace Xamarin_to_WinPhone.Views
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Xamarin_Forms/Xamarin_to_WinPhone/Xamarin_to_WinPhone/Xamarin_to_WinPhone/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using Xamarin_to_WinPhone.ViewModels;
+
+using Xamarin.Forms;
+
+namespace Xamarin_to_WinPhone.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+		{
+			InitializeComponent();
+
+			BindingContext = this.viewModel = viewModel;
+		}
+	}
+}
